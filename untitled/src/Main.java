c5d219fdc76f47f40a40a014293bffd8817a7c59
Professional <morewrost@gmail.com>
2022-11-14 23:09:02 +0300

    1.вывести на экран таблицу умножения на 3 2. Найти среднее арифметическое суммы чисел от 1 до 100

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,21 @@
+/*public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++)
+            System.out.println(i + " * 3 = " + i*3);
+    }
+}
+*/
+
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int result = 0;
+        for(int i=100; i>0; i--){
+            result = result+i;
+        }
+        System.out.println("My sum" + result/100);
+
     }
 }
